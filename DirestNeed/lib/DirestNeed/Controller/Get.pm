c326c40ca14282ba916c612e96c814c39836c9f6
Andrew John Dougherty <adougher9@gmail.com>
2020-10-15 10:39:44 -0400

    Fixed reverse geolocation for receivers.

--- DirestNeed/lib/DirestNeed/Controller/Get.pm
+++ DirestNeed/lib/DirestNeed/Controller/Get.pm
@@ -2,6 +2,8 @@ package DirestNeed::Controller::Get;
 use Moose;
 use namespace::autoclean;
 
+use File::Slurp;
+
 BEGIN { extends 'Catalyst::Controller'; }
 
 =head1 NAME
@@ -20,12 +22,15 @@ Catalyst Controller.
 
 sub index :Path :OptionalArgs(1) {
   my ( $self, $c, $receiver ) = @_;
+  my $google_maps_apikey = read_file($ENV{HOME}.'/.config/frdcsa/google_maps_apikey.txt');
+  chomp $google_maps_apikey;
   if (defined $receiver) {
     if (exists $c->model('DB')->roles->{$receiver}{receiver}) {
       $c->stash
 	(
 	 receiver => $receiver,
 	 shoppinglist => $c->model('DB')->shoppinglists->{$receiver},
+	 google_maps_apikey => $google_maps_apikey,
 	);
     }
   } else {
@@ -39,6 +44,16 @@ sub index :Path :OptionalArgs(1) {
 sub login :Local :Args(0) {
   my ( $self, $c ) = @_;
   $c->response->redirect($c->uri_for($c->request->params->{login}));
+}
+
+sub maps_test :Local :Args(0) {
+  my ( $self, $c ) = @_;
+  my $google_maps_apikey = read_file($ENV{HOME}.'/.config/frdcsa/google_maps_apikey.txt');
+  chomp $google_maps_apikey;
+  $c->stash
+    (
+     google_maps_apikey => $google_maps_apikey,
+    );
 }
 
 
